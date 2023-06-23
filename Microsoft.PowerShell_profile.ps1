73c258a3c1baf998afa0e4e3934092df96fbc9f5
Kevin <55131275+kevanoullio@users.noreply.github.com>
2023-06-23 02:00:02 -0600

    Added PowerShell_profile.ps1 file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+oh-my-posh init pwsh | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+& 'C:\Users\kevan\Miniconda3\shell\condabin\conda-hook.ps1'
+conda activate 'C:\Users\kevan\Miniconda3'
