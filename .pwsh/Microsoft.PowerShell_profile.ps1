3750aec81d3019cb030f4d3dd4bf74909def9cac
Kevin <55131275+kevanoullio@users.noreply.github.com>
2023-07-02 16:13:21 -0600

    Update Microsoft.PowerShell_profile.ps1
    Added an alias to navigate to WSL2 Ubuntu home/ directory

--- .pwsh/Microsoft.PowerShell_profile.ps1
+++ .pwsh/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,8 @@ oh-my-posh init pwsh | Invoke-Expression
 
 # Imports module that adds file and folder icons
 Import-Module -Name Terminal-Icons
+
+# Add an alias to easily navigate to my WSL2 Ubuntu home/ directory
+function GoUbuntuHome {
+    Set-Location "\\wsl$\Ubuntu\home\kevanoullio"
+}
