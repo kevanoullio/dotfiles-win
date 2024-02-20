e64e3bf579b38832c8d65888418ce84a8f175583
Kevin <55131275+kevanoullio@users.noreply.github.com>
2024-02-20 12:43:21 -0700

    Update Microsoft.PowerShell_profile.ps1
    Added own custom theme, borrowed and inspired by default OhMyPosh themes.

--- .pwsh/Microsoft.PowerShell_profile.ps1
+++ .pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 # Add the oh-my-posh theme
-oh-my-posh init pwsh | Invoke-Expression
+# oh-my-posh init pwsh | Invoke-Expression # NO THEME
+# oh-my-posh init pwsh --config "C:\Users\kevan\AppData\Local\Programs\oh-my-posh\themes\emodipt-extend.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "C:\Users\kevan\AppData\Local\Programs\oh-my-posh\themes\kushal.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "C:\Users\kevan\AppData\Local\Programs\oh-my-posh\themes\quick-term.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\kevan\AppData\Local\Programs\oh-my-posh\themes\kevanoullio.omp.json" | Invoke-Expression
 
 # Imports module that adds file and folder icons
 Import-Module -Name Terminal-Icons
